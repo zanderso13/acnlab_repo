c03ce88c52afe1859e7136bc006a178f08502dd9
Zachary Anderson <zaz3744@quser23.quest.it.northwestern.edu>
2020-01-15 13:18:42 -0600

    Pushing new scripts for fmriprep on the cluster

---
+++ preproc/single_sub_fmriprep.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+#SBATCH -A p30954
+#SBATCH -p normal
+#SBATCH -t 24:00:00
+#SBATCH --mem=64G
+#SBATCH -J fmriprep_test
+
+module purge
+module load singularity/latest
+echo "modules loaded" 
+cd /projects/b1108
+pwd
+echo "better run"
+
+singularity run --cleanenv /projects/b1108/software/singularity_images/fmriprep-1.5.4.simg /projects/b1108/data/BrainMAPD/ /projects/b1108/projects/BrainMAPD_func_conn/ participant --participant-label 10001 --fs-license-file ~/freesurfer_license/license.txt --fs-no-reconall -w /projects/b1108/projects/BrainMAPD_func_conn/work
