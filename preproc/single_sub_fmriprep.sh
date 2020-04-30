389a5ec09abcef942f14d25b701906e9c90763af
Sarah Etuk <sme0171@quser22.quest.it.northwestern.edu>
2020-04-30 16:05:08 -0500

    I added a task flag

--- preproc/single_sub_fmriprep.sh
+++ preproc/single_sub_fmriprep.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-#SBATCH -A p30954
+#SBATCH -A p30985
 #SBATCH -p normal
-#SBATCH -t 12:00:00
+#SBATCH -t 15:00:00
 #SBATCH --mem=64G
 #SBATCH -J fmriprep_test
 
@@ -13,4 +13,4 @@ cd /projects/b1108
 pwd
 echo "beginning preprocessing"
 
-singularity run --cleanenv /projects/b1108/software/singularity_images/fmriprep-1.5.4.simg /projects/b1108/data/BrainMAPD/ /projects/b1108/projects/BrainMAPD_func_conn/ participant --participant-label ${1} --fs-license-file ~/freesurfer_license/license.txt --fs-no-reconall -w /projects/b1108/projects/BrainMAPD_func_conn/work
+singularity run --cleanenv /projects/b1108/software/singularity_images/fmriprep-1.5.4.simg /projects/b1108/data/BrainMAPD/ /projects/b1108/projects/BrainMAPD_func_conn/ participant --participant-label ${1} -t REST --fs-license-file /projects/b1108/software/freesurfer_license/license.txt --fs-no-reconall -w /projects/b1108/projects/BrainMAPD_func_conn/work
