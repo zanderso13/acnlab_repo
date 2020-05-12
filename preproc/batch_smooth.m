ee0b9f30186100a178d473bfb5b63128155cb5a1
zanderso13 <zanderso13@gmail.com>
2020-05-11 20:33:04 -0500

    smoothing scripts

---
+++ preproc/batch_smooth.m
@@ -0,0 +1,9 @@
+% List of open inputs
+nrun = X; % enter the number of runs here
+jobfile = {'/Users/zaz3744/Documents/current_projects/ACNlab/BrainMAPD/smooth_template.m'};
+jobs = repmat(jobfile, 1, nrun);
+inputs = cell(0, nrun);
+for crun = 1:nrun
+end
+spm('defaults', 'FMRI');
+spm_jobman('run', jobs, inputs{:});
