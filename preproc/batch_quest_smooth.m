f650b4c60f255b1767e6a97ae742c0ed10071223
zanderso13 <32366377+zanderso13@users.noreply.github.com>
2023-05-19 15:45:59 -0500

    Update batch_quest_smooth.m

--- preproc/batch_quest_smooth.m
+++ preproc/batch_quest_smooth.m
@@ -1,45 +1,12 @@
-% batch script to submit many jobs. Initial version of this will be for my
-% local test of adding variables to the run_subject_firstlevel_MID.m
-% script. Then I will add components that will allow for integration with
-% the cluster.
-
-% Dependencies: spm12, CanlabCore, acnlab_repo
-
-% add paths
-% repodir = '~/Documents/repo';
-% addpath(genpath(repodir))
-
-% Define input to single sub first levels script
-% I think the best way to do this is going to be to find which files exist
-% and then defining variables relative to those paths. It's going to be
-% clunky and IT WILL CHANGE EVERYTIME YOU MOVE FILES AROUND. But since
-% we're in BIDS format... Maybe I can actually make this more dynamic. 
-
-%%%%%%% USER DEFINED %%%%%%%%%%
-% Define some paths
-% This is going to generate a first level script to be submitted to the
-% cluster with each run. Where do you want all these .sh scripts saved?
-scriptdir = '/projects/b1108/projects/BrainMAPD_func_conn/first_levels/quest_submission';
-
-% Where are all your scripts saved for first levels? i.e. where is the
-% acnlab_repo folder? Also where is spm12... you need spm
+scriptdir = '~/repo/acnlab_repo';
 
 repodir = '~/repo';
 
-% directories
-% first is where your stats files will be output to
-directories{1} = '/projects/b1108/projects/BrainMAPD_func_conn/first_levels/first_level_output';
 % next is where the preprocessed data is
-directories{2} = '/projects/b1108/projects/BrainMAPD_func_conn/fmriprep';
-% where the raw data lives (raw meaning before preprocessing)
-directories{3} = '/projects/b1108/data/BrainMAPD';
-% the timing files for modelling (onsets, durations, names)
-directories{4} = '/projects/b1108/projects/BrainMAPD_func_conn/timing_files';
-% where framewise displacement files will be saved
-directories{5} = '/projects/b1108/projects/BrainMAPD_func_conn/additional_files';
+directories{2} = '/projects/b1108/studies/brainmapd/data/processed/neuroimaging/fmriprep/';
 
 % What run of your task are you looking at?
-run = 2;
+run = 1;
 % What session appears in your raw filenames when in BIDS format?
 ses = 2;
 % Do you want to overwrite previously estimated first levels or just add to
@@ -49,20 +16,12 @@ overwrite = 0;
 % i.e. 10234 would correspond with a 5 for this variable
 ID_length = 5;
 
-%%%%%%% END USER DEFINED %%%%%%%%%%
 
-% This will give you PID, don't change this.
-% foldernames = char(filenames(fullfile(directories{2},'sub*/')));
-% sublist = foldernames(:,size(foldernames,2)-(ID_length):size(foldernames,2)-1);
-% 
-% sublist = string(sublist);
-% I like this better than the overwrite option. This way, the list of
-% subjects to be run will change each time I run the script. The overwrite
-% option will still be helpful here to turn things on or off.
 file_list = filenames(fullfile(directories{2},strcat('*/ses-',num2str(ses),'/func/sub*MID*run',num2str(run),'*preproc_bold.nii')));
 for i = 1:length(file_list)
     sublist{i} = file_list{i}(59:63);
 end
+keyboard
 sublist = string(sublist);
 if overwrite == 0
     smooth_list = filenames(fullfile(directories{2},strcat('*/ses-',num2str(ses),'/func/ssub*MID*run*',num2str(run),'*')));
